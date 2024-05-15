50dbf56a059d08bc033e43b0422b55ee6bc40ff8
TeddyLondon17 <alexanderstrife17@gmail.com>
2024-05-15 15:03:00 -0500

    upadted knex file

--- Boilerplate-Backend/knexfile.js
+++ Boilerplate-Backend/knexfile.js
@@ -1,9 +1,7 @@
 const path = require("path");
 require("dotenv").config();
 const {
-  PROD_DATABASE_URL = "postgres://wgqqvvdc:2-W8dDk6kG4UZHsZOjWb5Ec2zFBTytNJ@stampy.db.elephantsql.com/wgqqvvdc",
-  DEV_DATABASE_URL = "postgres://ghtpnves:wKOkGAu311nc4o75x7gb0IQc_0PoqMGu@stampy.db.elephantsql.com/ghtpnves",
-} = process.env;
+  PROD_DATABASE_URL,DEV_DATABASE_URL} = process.env;
 
 module.exports = {
   development: {
